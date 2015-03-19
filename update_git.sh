a8155916e65434b06778bc057f7d2439a6da9698
Anjishnu Kumar <anjikum@amazon.com>
2015-03-19 11:25:38 -0700

    latest update

--- update_git.sh
+++ update_git.sh
@@ -1,4 +1,14 @@
 ./sample_cmd.sh
+CALLING_DIR=`pwd`
+#DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DIR=`dirname "$0"`
+cd $DIR
 git add *
 git commit -m "latest update"
 git push origin master
+cd $CALLING_DIR
+
+#Remember to change your git to not require a username and password
+#Do crontab -e and add this script
+# 00, 11,23, * * * /path/to/this/script
+# This will push to github twice a day (11 and 23) without doing any checks 
